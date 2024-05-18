5b5a9779841864d1b7a8cfdfdcb186c4f3ed5ab8
Park Juhyung <me@juhyung.dev>
2024-05-18 22:35:46 +0900

    영상 싱크 시간을 홈에 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,24 @@
 import { route } from 'preact-router';
+import { useEffect, useState } from 'preact/hooks';
 
 export function Home({ ...props }) {
+  const [count, setCount] = useState(0);
+
   const handleClick = (movieId: number) => {
     route(`/movie/${movieId}`);
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(new Date().getSeconds());
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+
   return <div>
     <h1>Home</h1>
     <button onClick={() => handleClick(1)}> 첫번째 패드 </button>
@@ -13,6 +28,10 @@ export function Home({ ...props }) {
     <button onClick={() => handleClick(4)}> 네번째 패드 </button>
     <button onClick={() => handleClick(5)}> 다섯번째 패드 </button>
     <button onClick={() => handleClick(6)}> 여섯번째 패드 </button>
+    <br />
+    <p>
+      영상 싱크: {count}
+    </p>
     <pre>{JSON.stringify(props, null, 2)}</pre>
   </div>
 }
