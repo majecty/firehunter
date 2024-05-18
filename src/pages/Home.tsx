1efa10bc38a0489bf37a7354a659044c2185f2df
Park Juhyung <me@juhyung.dev>
2024-05-18 22:29:54 +0900

    홈페이지에 6개 버튼 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,18 @@
 import { route } from 'preact-router';
 
 export function Home({ ...props }) {
-  const handleClick = () => {
-    route('/movie/1');
+  const handleClick = (movieId: number) => {
+    route(`/movie/${movieId}`);
   };
   return <div>
     <h1>Home</h1>
-    <button onClick={handleClick}> 첫번째 패드 </button>
+    <button onClick={() => handleClick(1)}> 첫번째 패드 </button>
+    <button onClick={() => handleClick(2)}> 두번째 패드 </button>
+    <button onClick={() => handleClick(3)}> 세번째 패드 </button>
+    <br />
+    <button onClick={() => handleClick(4)}> 네번째 패드 </button>
+    <button onClick={() => handleClick(5)}> 다섯번째 패드 </button>
+    <button onClick={() => handleClick(6)}> 여섯번째 패드 </button>
     <pre>{JSON.stringify(props, null, 2)}</pre>
   </div>
 }
