195bbe5a50e7d935c7745a69b20d34938fdcf40a
Park Juhyung <me@juhyung.dev>
2024-05-18 22:37:16 +0900

    movie에 홈으로 돌아가기 버튼 추가

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -1,8 +1,10 @@
+import { route } from "preact-router";
 
 export function Movie({ ...props }) {
 
   return <div>
     <h1>Movie {props.id}</h1>
+    <button onClick={() => route('/')}>Home으로 돌아가기</button>
     <pre>{JSON.stringify(props, null, 2)}</pre>
   </div>
 }
