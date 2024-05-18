76442bf06640bbf22cc6d1364db8c3afe2786273
Park Juhyung <me@juhyung.dev>
2024-05-18 23:37:19 +0900

    video sphere 사용

--- src/pages/AframeSample.tsx
+++ src/pages/AframeSample.tsx
@@ -3,17 +3,30 @@ import "aframe";
 // @ts-ignore
 import { Entity, Scene } from "aframe-react";
 
-export function AframeSample({...props}) {
+const sampleVideoUrl = "https://firehunter.s3.ap-northeast-2.amazonaws.com/0518sample.mp4";
+
+declare module "preact" {
+  namespace JSX {
+    interface IntrinsicElements {
+      "a-videosphere": any;
+      "a-assets": any;
+    }
+  }
+}
+
+export function AframeSample({ ...props }) {
+  console.log(props);
+
   return <div>
     <h1>AframeSample</h1>
     <button onClick={() => route('/')}>Home으로 돌아가기</button>
     <Scene>
-      <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
-      <Entity geometry={{primitive: 'sphere'}} material={{color: 'blue'}} position={{x: 2, y: 0, z: -5}}/>
-      <Entity geometry={{primitive: 'cylinder'}} material={{color: 'green'}} position={{x: -2, y: 0, z: -5}}/>
-      <Entity light={{type: 'point'}}/>
-      <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
-      <Entity text={{value: 'Hello, WebVR!'}}/>
+      <a-assets>
+        <video id="sample-video" autoplay loop={true}
+          src={sampleVideoUrl} crossorigin="anonymous"
+        />
+      </a-assets>
+      <a-videosphere src="#sample-video"></a-videosphere>
     </Scene>
   </div>
 }
