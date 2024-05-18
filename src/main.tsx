bc0ab5f2969c60e018e21387615477be0e5edda4
Park Juhyung <me@juhyung.dev>
2024-05-19 00:34:55 +0900

    버전 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import { render } from 'preact'
 import { App } from './app.tsx'
 import './index.css'
 
+const version = '1.0.1'
+console.log(`App version: ${version}`)
+
 render(<App />, document.getElementById('app')!)
