84883a7dad25803c12686c47c8ce20cd788881ed
Park Juhyung <me@juhyung.dev>
2024-05-18 22:38:18 +0900

    영상 페이지에 싱크 텍스트 추가

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -1,10 +1,26 @@
 import { route } from "preact-router";
+import { useEffect, useState } from "preact/hooks";
 
 export function Movie({ ...props }) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(new Date().getSeconds());
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
 
   return <div>
     <h1>Movie {props.id}</h1>
     <button onClick={() => route('/')}>Home으로 돌아가기</button>
+    <p>
+      영상 싱크: {count}
+    </p>
     <pre>{JSON.stringify(props, null, 2)}</pre>
   </div>
 }
