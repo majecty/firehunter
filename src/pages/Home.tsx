a80d776ea0ba54c5c5629138828fe00d78c11a8b
Park Juhyung <me@juhyung.dev>
2024-05-18 22:28:29 +0900

    홈 페이지에 첫번째 버튼 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
+import { route } from 'preact-router';
 
 export function Home({ ...props }) {
+  const handleClick = () => {
+    route('/movie/1');
+  };
   return <div>
     <h1>Home</h1>
+    <button onClick={handleClick}> 첫번째 패드 </button>
     <pre>{JSON.stringify(props, null, 2)}</pre>
   </div>
 }
