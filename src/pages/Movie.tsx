e2c7f234ded104dd1b714c87de85dd824296c245
juhyung <me@juhyung.dev>
2024-06-01 15:33:10 +0900

    fov 추가

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -10,6 +10,7 @@ export function Movie({ ...props }) {
   const [current, setCurrent] = useState(0);
   const videoRef = useRef<HTMLVideoElement>(null);
   const [enableSync, setEnableSync] = useState(true);
+  const [fov, setFov] = useState(80);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -54,6 +55,7 @@ export function Movie({ ...props }) {
     <button onClick={() => route('/')}>Home으로 돌아가기</button>
     <p>
       영상 싱크: {target}
+      FOV: {fov}
     </p>
     <pre>{JSON.stringify(props, null, 2)}</pre>
 
@@ -65,10 +67,13 @@ export function Movie({ ...props }) {
         />
       </a-assets>
       <a-videosphere src="#sample-video"></a-videosphere>
+      <a-camera fov={fov.toString()}>  </a-camera>
     </Scene>
 
     <p style={{ position: "absolute", top: "70%", left: "50%", transform: "translate(-50%, -50%)", zIndex: "9999", color: "white", backgroundColor: "black" }}>{props.id}번째 타블렛 싱크 목표: {target} 현재: {current}</p>
     <button style={{ position: "absolute", top: "90%", left: "50%", transform: "translate(-50%, -50%)", zIndex: "9999"}} onClick={() => setEnableSync(!enableSync)}>싱크 {enableSync ? "끄기" : "켜기"}</button>
+    <p style={{ position: "absolute", top: "95%", left: "50%", transform: "translate(-50%, -50%)", zIndex: "9999", color: "white", backgroundColor: "black" }}>FOV</p>
+    <input type="range" min="30" max="120" value={fov} onChange={(e) => setFov(parseInt((e.target! as any).value))} style={{ position: "absolute", top: "95%", left: "50%", transform: "translate(-50%, -50%)", zIndex: "9999"}} />
     {videoRef.current !== null && videoRef.current.paused && <button style={{ position: "absolute", top: "95%", left: "50%", transform: "translate(-50%, -50%)", zIndex: "9999"}} onClick={() => videoRef.current!.play()}>재생</button>}
   </div>
 }
