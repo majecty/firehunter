117c28f7418bda28511ecf31b2830b035d4ef0ce
juhyung <me@juhyung.dev>
2024-06-01 21:42:57 +0900

    두번째 시도 home 만듬

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import Router from 'preact-router'
 import { Home } from './pages/firsttry/Home'
 import { Movie } from './pages/firsttry/Movie'
 import { AframeSample } from './pages/firsttry/AframeSample'
+import { Home2 } from './pages/secondtry/SecondHome'
 
 export function App() {
   return (
@@ -11,6 +12,8 @@ export function App() {
       <Home path="/"/>
       <Movie path="/movie/:id"/>
       <AframeSample path="/aframe"/>
+
+      <Home2 path="/second"/>
     </Router>
     </>
   )
---
+++ src/pages/secondtry/SecondHome.tsx
@@ -0,0 +1,37 @@
+import { route } from 'preact-router';
+import { useEffect, useState } from 'preact/hooks';
+
+export function Home2({ ...props }) {
+  const [count, setCount] = useState(0);
+
+  const handleClick = (movieId: number) => {
+    route(`/second/movie/${movieId}`);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(new Date().getSeconds());
+    }, 100);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+
+  return <div>
+    <h1>Home2</h1>
+    <button onClick={() => handleClick(1)}> 첫번째 패드 </button>
+    <button onClick={() => handleClick(2)}> 두번째 패드 </button>
+    <button onClick={() => handleClick(3)}> 세번째 패드 </button>
+    <br />
+    <button onClick={() => handleClick(4)}> 네번째 패드 </button>
+    <button onClick={() => handleClick(5)}> 다섯번째 패드 </button>
+    <button onClick={() => handleClick(6)}> 여섯번째 패드 </button>
+    <br />
+    <p>
+      영상 싱크: {count}
+    </p>
+    <pre>{JSON.stringify(props, null, 2)}</pre>
+  </div>
+}
