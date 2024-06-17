0ac9b3e31b4ef22fe712da912cee02a651484818
juhyung <me@juhyung.dev>
2024-06-17 20:09:47 +0900

    hls test 비디오 s3에 업로드

--- src/pages/fourthtry/HLSTest.tsx
+++ src/pages/fourthtry/HLSTest.tsx
@@ -3,8 +3,9 @@ export function HLSTest({ ...props }) {
 
   return <div>
     <h2>HLSTest</h2>
+    // https://gist.github.com/lukebussey/4d27678c72580aeb660c19a6fb73e9ee
     <video controls>
-      <source src="http://localhost:8000/0518sample.m3u8" type="application/x-mpegURL"/>
+      <source src="https://firehunter.s3.ap-northeast-2.amazonaws.com/0518hls/0518sample.m3u8" type="application/x-mpegURL"/>
     </video>
   </div>
 }
