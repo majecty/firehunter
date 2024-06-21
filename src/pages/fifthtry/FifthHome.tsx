2b3d0cdeec1ce605e286125ace957e6f370853c2
juhyung <me@juhyung.dev>
2024-06-21 21:11:27 +0900

    다섯번째 영상 분기 버튼 만듬

--- src/pages/fifthtry/FifthHome.tsx
+++ src/pages/fifthtry/FifthHome.tsx
@@ -4,5 +4,14 @@ export function FifthHome({ ...props }) {
 
   return <div>
     <h1>다섯번째 시도</h1>
+    <p>
+      원본 영상과 같은 품질의 영상을 2초로 잘라서 360도로 무한재생합니다.
+      iphone SE2에서 TV로 airplay했을 때 영상의 품질을 확인하기 위함입니다.
+    </p>
+    <button>750MB 원본을 2초로 자른 영상 테스트</button>
+    <button>700MB 원본을 2초로 자른 영상 테스트</button>
+    <button>400MB 원본을 2초로 자른 영상 테스트</button>
+    <button>300MB 원본을 2초로 자른 영상 테스트</button>
+    <button>200MB 원본을 2초로 자른 영상 테스트</button>
   </div>
 }
