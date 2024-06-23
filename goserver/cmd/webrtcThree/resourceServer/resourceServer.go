a3b0414a8c5be161cee67256cc685fbf9a800096
juhyung <me@juhyung.dev>
2024-06-23 17:30:20 +0900

    background 추가

--- goserver/cmd/webrtcThree/resourceServer/resourceServer.go
+++ goserver/cmd/webrtcThree/resourceServer/resourceServer.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+)
 
 func main() {
 	fmt.Println("resourceServer start")
+	ctx := context.Background()
+	if err := webrtcMain(ctx); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+}
+
+func webrtcMain(ctx context.Context) error {
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
+	fmt.Println("webrtcMain start")
+
+	<-ctx.Done()
+	fmt.Println("webrtcMain end")
+	return nil
 }
